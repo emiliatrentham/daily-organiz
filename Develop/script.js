f6953d5c0f0b9119f7b41611ba5cb23ff35da6ab
Emilia Trentham <lavernatrentham@gmail.com>
2022-10-27 14:16:35 -0600

    Adds gitignore file

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,24 @@
+// const timeBlocksList = document.getElementById("time-blocks-list");
+// const currentDay = document.getElementById("currentDay");
+
+// // Get Current Time and display it every second
+// function getTime() {
+//     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+//     currentDay.innerHTML = currentDate;
+//     currentDay.innerHTML = "current date: " + currentDate;
+// }
+// setInterval(getTime, 1000);
+
+// // Temporary timeblocks
+// let timeblocks = [
+//     "6:30 AM",
+//     "7:30 AM",
+//     "8:30 AM",
+//     "9:30 AM",
+//     "10:30 AM",
+//     "11:30 AM",
+//     "12:30 PM"  
+// ]
 
 const currentDate = moment().format("dddd, MMMM Do"); // get current date
 document.getElementById("currentDay").textContent = currentDate; // display inside the element
