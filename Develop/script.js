2386d93f899d87d80330da08d2ab5186f414fcdd
Emilia Trentham <lavernatrentham@gmail.com>
2022-10-31 11:13:42 -0600

    Adds clear history button

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,52 @@
-// const timeBlocksList = document.getElementById("time-blocks-list");
-// const currentDay = document.getElementById("currentDay");
-
-// // Get Current Time and display it every second
-// function getTime() {
-//     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-//     currentDay.innerHTML = currentDate;
-//     currentDay.innerHTML = "current date: " + currentDate;
-// }
-// setInterval(getTime, 1000);
-
-// // Temporary timeblocks
-// let timeblocks = [
-//     "6:30 AM",
-//     "7:30 AM",
-//     "8:30 AM",
-//     "9:30 AM",
-//     "10:30 AM",
-//     "11:30 AM",
-//     "12:30 PM"  
-// ]
+var clearButton = document.querySelector(".clear");
+var timeBlockHourKey = document.querySelector(".description");
 
 const currentDate = moment().format("dddd, MMMM Do"); // get current date
 document.getElementById("currentDay").textContent = currentDate; // display inside the element
 
-$(".saveBtn").click(function() {
-    let $siblingTextarea = $(this).siblings("textarea")
-    let textareaValue = $siblingTextarea.val()
-    console.log(textareaValue)
-    let $parentTimeBlock = $(this).closest(".time-block")
-    let timeBlockHourKey = $parentTimeBlock.data("hour")
-    saveToLocalStorage(timeBlockHourKey, textareaValue)
-}); 
+const now = moment().format("HH:mm A"); // figure out how to show am pm and how to compare times in moment
+console.log(now);
 
-function saveToLocalStorage(hourKey, descriptionText) {
-    localStorage.setItem(hourKey, descriptionText)
-};
-
-$(document).ready(function() {
-    $(".time-block").each(function () {
-        let timeBlockHourKey = $(this).data("hour")
-        // dataset.hour vanilla js
-        let existingDescription = localStorage.getItem(timeBlockHourKey)
-        $(this).find("textarea").val(existingDescription) //chaining
-    }) 
+// The save button function that allows the user to save their notes
+$(".saveBtn").click(function () {
+  let $siblingTextarea = $(this).siblings("textarea");
+  let textareaValue = $siblingTextarea.val();
+  console.log(textareaValue);
+  let $parentTimeBlock = $(this).closest(".time-block");
+  let timeBlockHourKey = $parentTimeBlock.data("hour");
+  saveToLocalStorage(timeBlockHourKey, textareaValue);
 });
 
+function saveToLocalStorage(hourKey, descriptionText) {
+  localStorage.setItem(hourKey, descriptionText);
+}
+// Fetching items from local storage
+$(document).ready(function () {
+  // console.log($(".time-block"))
+  $(".time-block").each(function () {
+    // console.log("timeBlock", this);
+    let timeBlockHourKey = $(this).data("hour");
+    // dataset.hour vanilla js
+    let existingDescription = localStorage.getItem(timeBlockHourKey);
+    $(this).find("textarea").val(existingDescription); //chaining the result of a function to the front of another
+    // console.log(timeBlockHourKey, now);
 
-// use js to add past present future
-// inside timeblock.each use moment to find out if each time block represents before or after present 
+    var updateTimeBlockTime = moment(timeBlockHourKey, "HH:mm A").format(
+      "HH:mm A"
+    );
+    if (updateTimeBlockTime < now) {
+      $(this).addClass("past");
+    } else if (updateTimeBlockTime > now) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+});
+
+function resetTextArea() {
+  localStorage.clear();
+  timeBlockHourKey.descriptionText = "";
+}
+clearButton.addEventListener("click", resetTextArea);
+
