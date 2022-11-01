30c49ec4ad338b529219a7f7609f8f608776e3ba
Emilia Trentham <lavernatrentham@gmail.com>
2022-10-31 18:47:19 -0600

    Updates code comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,12 @@
+// Defines query selectors
 var clearButton = document.querySelector(".clear");
 var timeBlockHourKey = document.querySelector(".description");
-
+// Sets criteria to display current date using moment 
 const currentDate = moment().format("dddd, MMMM Do"); // get current date
 document.getElementById("currentDay").textContent = currentDate; // display inside the element
 
-const now = moment().hour(); 
-console.log(now);
+// Variable used to update time block color based on the time of day
+const now = moment().hour();
 
 // The save button function that allows the user to save their notes
 $(".saveBtn").click(function () {
@@ -17,22 +18,21 @@ $(".saveBtn").click(function () {
   saveToLocalStorage(timeBlockHourKey, textareaValue);
 });
 
+// Sets criteria to save event text to local storage
 function saveToLocalStorage(hourKey, descriptionText) {
   localStorage.setItem(hourKey, descriptionText);
 }
 // Fetching items from local storage
 $(document).ready(function () {
-  // console.log($(".time-block"))
   $(".time-block").each(function () {
     // console.log("timeBlock", this);
     let timeBlockHourKey = $(this).data("hour");
-    // dataset.hour vanilla js
     let existingDescription = localStorage.getItem(timeBlockHourKey);
-    $(this).find("textarea").val(existingDescription); //chaining the result of a function to the front of another
-    // console.log(timeBlockHourKey, now);
-
-    var updateTimeBlockTime = moment(timeBlockHourKey, "HH:mm A").hour(
-    );
+    $(this).find("textarea").val(existingDescription);
+    //chaining the result of a function to the front of another
+   
+    // Sets criteria for time blocks to change color based on the time of day
+    var updateTimeBlockTime = moment(timeBlockHourKey, "HH:mm A").hour();
     if (updateTimeBlockTime < now) {
       $(this).addClass("past");
     } else if (updateTimeBlockTime > now) {
@@ -43,15 +43,14 @@ $(document).ready(function () {
   });
 });
 
+// Sets clear history button criteria
 function resetTextArea() {
   localStorage.clear();
-    timeBlockHourKey.descriptionText = "";
-    $(".time-block").each(function () {
-      let timeBlockHourKey = $(this).data("hour");
-      let existingDescription = localStorage.getItem(timeBlockHourKey);
-      $(this).find("textarea").val(existingDescription); 
-    });
-    
+  timeBlockHourKey.descriptionText = "";
+  $(".time-block").each(function () {
+    let timeBlockHourKey = $(this).data("hour");
+    let existingDescription = localStorage.getItem(timeBlockHourKey);
+    $(this).find("textarea").val(existingDescription);
+  });
 }
 clearButton.addEventListener("click", resetTextArea);
-
